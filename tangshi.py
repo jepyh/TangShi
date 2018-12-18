034a86435e8d394698868ec420ab8cede3ecfd1f
jepyh <41173467+jepyh@users.noreply.github.com>
2018-12-18 16:49:14 +0800

    Add files via upload

---
+++ tangshi.py
@@ -0,0 +1,22 @@
+import re
+from collections import Counter
+
+dic = []
+data = open('..//..//datasets//tangshi//tangshi.txt','r')  #下载的唐诗三百首
+
+for i in data.readlines():
+    dic.append(i.strip().replace('：','').replace('，','').replace('。','').replace('？','').replace('！','').replace('［','').replace('］',''))  #去除一些不用的符号
+word = ''.join(dic)
+word_str = re.sub(r"(?<=\w)","",word)  #把所有文字分开
+word_list = list(word_str)
+a = [v for v in word_list if not str(v).isdigit()]  #删除数字
+
+c = Counter(a)
+s = c.most_common(300)  #取了前300个最常见的汉字
+tangshi = []
+for i in s:
+    tangshi.append(i[0])
+
+import random
+i = random.sample(tangshi,20)  #从300个最常见的汉字中拿出20个
+print(i[0]+i[1]+i[2]+i[3]+i[4]+'，'+i[5]+i[6]+i[7]+i[8]+i[9]+'。'+'\n'+i[10]+i[11]+i[12]+i[13]+i[14]+'，'+i[15]+i[16]+i[17]+i[18]+i[19]+'。')
