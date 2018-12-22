5cd669e3d127206bd792d82d05557c0d941143ce
jepyh <41173467+jepyh@users.noreply.github.com>
2018-12-22 18:15:42 +0800

    Add files via upload

--- tangshi.py
+++ tangshi.py
@@ -1,8 +1,22 @@
 import re
 from collections import Counter
 
+import json
+
+
+with open(".//json//poet.tang.15000.json",'rb') as load_f:
+    load_dict = json.load(load_f)
+    print(load_dict)
+#load_dict['paragraphs'] = [8200,{1:[['Python',81]]}]
+#print(load_dict)
+
+#with open(".//json//poet.tang.15000.json","w") as dump_f:
+ #   json.dump(load_dict,dump_f)
+
+
+
 dic = []
-data = open('..//..//datasets//tangshi//tangshi.txt','r')  #下载的唐诗三百首
+data = open('..//..//datasets//tangshi//biansaishi.txt','r')  #下载的唐诗三百首tangshi.txt
 
 for i in data.readlines():
     dic.append(i.strip().replace('：','').replace('，','').replace('。','').replace('？','').replace('！','').replace('［','').replace('］',''))  #去除一些不用的符号
